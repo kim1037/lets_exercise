ef1592e700382520460d8d67d569f7911c82d083
kim1037 <pan913063@gmail.com>
2023-12-25 20:31:43 +0800

    feat: add create database script

--- utils/db-mysql.js
+++ utils/db-mysql.js
@@ -1,60 +1,42 @@
-const fs = require('fs')
-const mysql = require('mysql')
-let config = {}
-
-
+const mysql = require('mysql2')
 
 const db = {
-  createDB: (config={}) => {
-    console.log('config.mysql:', config)
+  createDB: (config = {}) => {
     return new Promise((resolve, reject) => {
       if (!config.database) {
-        reject('DatabaseName is required!')
+        reject(new Error('DatabaseName is required!'))
         return
       }
-      
+
       let sql = ''
       if (config.charset && config.collation) {
         sql = `CREATE SCHEMA ${config.database} DEFAULT CHARACTER SET ${config.charset} COLLATE ${config.collation};`
       } else {
         sql = `CREATE DATABASE ${config.database};`
       }
-      
+
       const conn = mysql.createConnection({
         host: config.host,
         user: config.user,
         password: config.password
       })
+      console.log('DB is connecting.')
 
-      conn.connect((err) => {
-        console.log('DB is connecting.')
+      conn.query(sql, (err, results, fields) => {
+        conn.end() // close connection
         if (err) {
-          reject(err);
-          return;
+          reject(err)
+        } else {
+          resolve('Database has created')
         }
-        conn.query(sql, (queryErr, result) => {
-          console.log('DB is creating database.')
-          conn.end(); // close connection
-          if (queryErr) {
-            reject(queryErr);
-          } else {
-            resolve('Database created');
-          }
-        });
       })
     })
   },
-  pool : (config={})=>{
-    const dbPool = mysql.createPool({config})
+  pool: (config = {}) => {
+    const dbPool = mysql.createPool({ config })
     return this
   }
 
 }
 
 module.exports = db
-// read config data then trans it to obj
-fs.readFile('config/config.json', 'utf8', (err, data) => {
-  if (err) return console.error(err)
-  config = JSON.parse(data)
-  db.createDB(config.mysql).then(result=> console.log(result)).catch(err=>console.error(err))
-})
