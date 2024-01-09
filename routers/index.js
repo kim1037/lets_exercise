4355e23cbeb012676d1f21449692ae4e0669b343
kim1037 <pan913063@gmail.com>
2024-01-09 22:19:24 +0800

    feat: add error handler

--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,19 @@
 const router = require('express').Router()
 const users = require('./modules/users')
 
-router.use('/api/users', users)
+// 註冊、登入不必驗證
+router.post('/users/signup')
+router.post('/users/signin')
+// 加入驗證
+router.use('/users', users)
 
+// error handle
+router.use('/', function (err, req, res, next) {
+  const code = err.status || 500
+  res.status(code).json({
+    status: 'error',
+    statusCode: code,
+    error: `${err.message}`
+  })
+})
 module.exports = router
