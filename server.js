0d4ccd778bdb2320fc971825c4e0ac3272010642
kim1037 <pan913063@gmail.com>
2024-02-20 22:38:36 +0800

    feat: add cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 global.config = require('./config/config')
 const PORT = global.config.PORT || 3000
+const cors = require('cors')
 
 // connent database
 const mysql = require('mysql2/promise')
@@ -12,9 +13,15 @@ global.pool = mysql.createPool({
   password: global.config.mysql.password,
   database: global.config.mysql.database
 })
-
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+// cors
+app.use(cors({
+  origin:'*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
+}))
 
 // set passport
 app.use(require('./config/passport').initialize())
