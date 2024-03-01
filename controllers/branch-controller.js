e069f2244edaba078bc067db14a4ee0fe0fd7b35
kim1037 <pan913063@gmail.com>
2024-03-01 10:57:43 +0800

    feat: add get users activities and participants api wait for testing

--- controllers/branch-controller.js
+++ controllers/branch-controller.js
@@ -1,13 +1,25 @@
+const { getOffset, getPagination } = require('../utils/paginator-helper')
 const branchController = {
   getAll: async (req, res, next) => {
     let connection
+    const page = Number(req.query.page) || 1 // 初始預設頁
+    const limit = Number(req.query.limit) || 10 // default 每頁10筆
+    const offset = getOffset(limit, page)
     try {
       connection = await global.pool.getConnection()
-      const [branches] = await connection.query('SELECT * FROM branches')
+      // find the total numbers of branches
+      const [amount] = await connection.query(`SELECT COUNT(*) AS total FROM branches `)
+      const totalAmount = amount[0].total
+      if (offset > totalAmount) {
+        const err = new Error(`資料頁碼超過範圍，此條件只有${Math.ceil(totalAmount / limit)}頁`)
+        err.status = 404
+        throw err
+      }
+      const [branches] = await connection.query(`SELECT * FROM branches LIMIT ${limit} OFFSET ${offset}`)
       if (!branches || branches.length === 0) {
         return res.status(200).json({ status: 'Success', message: '目前尚未有任何品牌的資訊' })
       }else{
-        return res.status(200).json({ status: 'Success', data: branches })
+        return res.status(200).json({ status: 'Success', pagination: getPagination(limit, page, totalAmount), data: branches })
       }
     } catch (err) {
       next(err)
--- controllers/shuttlecock-controller.js
+++ controllers/shuttlecock-controller.js
@@ -1,13 +1,25 @@
+const { getOffset, getPagination } = require('../utils/paginator-helper')
 const shuttlecockController = {
   getAll: async (req, res, next) => {
     let connection
+    const page = Number(req.query.page) || 1 // 初始預設頁
+    const limit = Number(req.query.limit) || 10 // default 每頁10筆
+    const offset = getOffset(limit, page)
     try {
       connection = await global.pool.getConnection()
-      const [shuttlecocks] = await connection.query('SELECT s.*, b.name FROM shuttlecocks AS s JOIN branches AS b ON b.id = s.branchId')
+      // find the total numbers of shuttlecocks
+      const [amount] = await connection.query(`SELECT COUNT(*) AS total FROM shuttlecocks `)
+      const totalAmount = amount[0].total
+      if (offset > totalAmount) {
+        const err = new Error(`資料頁碼超過範圍，此條件只有${Math.ceil(totalAmount / limit)}頁`)
+        err.status = 404
+        throw err
+      }
+      const [shuttlecocks] = await connection.query(`SELECT s.*, b.name FROM shuttlecocks AS s JOIN branches AS b ON b.id = s.branchId LIMIT ${limit} OFFSET ${offset}`)
       if (!shuttlecocks || shuttlecocks.length === 0) {
         return res.status(200).json({ status: 'Success', message: '目前尚未有任何羽球型號的資訊' })
       } else {
-        return res.status(200).json({ status: 'Success', data: shuttlecocks })
+        return res.status(200).json({ status: 'Success', pagination: getPagination(limit, page, totalAmount), data: shuttlecocks })
       }
     } catch (err) {
       next(err)
