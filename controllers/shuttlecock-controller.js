0d13937a09fe90e5da1d3ddee3720e6127f3cf02
kim1037 <pan913063@gmail.com>
2024-06-09 22:26:50 +0800

    fix: shuttlecock name bug

--- controllers/shuttlecock-controller.js
+++ controllers/shuttlecock-controller.js
@@ -18,7 +18,7 @@ const shuttlecockController = {
         err.status = 404
         throw err
       }
-      const [shuttlecocks] = await connection.query(`SELECT s.*, b.name FROM shuttlecocks AS s JOIN branches AS b ON b.id = s.branchId LIMIT ${limit} OFFSET ${offset}`)
+      const [shuttlecocks] = await connection.query(`SELECT s.*, b.name as branch FROM shuttlecocks AS s JOIN branches AS b ON b.id = s.branchId LIMIT ${limit} OFFSET ${offset}`)
       if (!shuttlecocks || shuttlecocks.length === 0) {
         return res.status(200).json({ status: 'Success', message: '目前尚未有任何羽球型號的資訊' })
       } else {
@@ -40,7 +40,7 @@ const shuttlecockController = {
     const { shuttlecockId } = req.params
     try {
       connection = await global.pool.getConnection()
-      const [shuttlecock] = await connection.query('SELECT s.*, b.name FROM shuttlecocks AS s JOIN branches AS b ON b.id = s.branchId WHERE s.id = ?', [shuttlecockId])
+      const [shuttlecock] = await connection.query('SELECT s.*, b.name as branch FROM shuttlecocks AS s JOIN branches AS b ON b.id = s.branchId WHERE s.id = ?', [shuttlecockId])
       if (!shuttlecock || shuttlecock.length === 0) {
         const err = new Error('找不到此羽球型號!')
         err.status = 404
