4dd79be500c8cb8fad6aa05ed1695ece3dee47b7
kim1037 <107454420+kim1037@users.noreply.github.com>
2024-06-10 22:41:49 +0800

    fix: get off 'get all arenas api' page default

--- controllers/arena-controller.js
+++ controllers/arena-controller.js
@@ -5,15 +5,15 @@ const arenaController = {
     // #swagger.description = '取得所有場地資訊'
 
     let connection
-    const page = Number(req.query.page) || 1 // 初始預設頁
-    const limit = Number(req.query.limit) || 10 // default 每頁10筆
     const regionId = req.query.regionId || '' // 篩選: 依縣市篩
-    const offset = getOffset(limit, page)
     try {
       connection = await global.pool.getConnection()
       // find the total numbers of arenas
       const [amount] = await connection.query(`SELECT COUNT(*) AS total FROM arenas ${regionId ? 'WHERE regionId = ?' : ''}`, (regionId ? [regionId] : []))
       const totalAmount = amount[0].total
+      const page = Number(req.query.page) || 1 // 初始預設頁
+      const limit = Number(req.query.limit) || totalAmount 
+      const offset = getOffset(limit, page)
       if (offset > totalAmount) {
         const err = new Error(`資料頁碼超過範圍，此條件只有${Math.ceil(totalAmount / limit)}頁`)
         err.status = 404
@@ -21,6 +21,7 @@ const arenaController = {
       }
       // get arenas data
       const [arenas] = await connection.query(`SELECT a.*, r.region FROM arenas AS a JOIN regions AS r ON a.regionId = r.id ${regionId ? 'WHERE a.regionId = ?' : ''} LIMIT ${limit} OFFSET ${offset}`, (regionId ? [regionId] : []))
+      console.log(arenas.length)
       if (!arenas || arenas.length === 0) {
         return res.status(200).json({ status: 'Success', message: '此條件查無場地' })
       } else {
